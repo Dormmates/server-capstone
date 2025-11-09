9304ffc9b44e10b6dd5d3a7c284a2c4cd45d6b98
Giovanni Leo <2232254@slu.edu.ph>
2025-11-09 11:45:59 +0800

    time issue

--- utils/convert.utils.js
+++ utils/convert.utils.js
@@ -1,10 +1,9 @@
 export const convertDates = (dates) => {
   return dates.map(({ date, time }) => {
-    const parsedDate = new Date(date);
     const [hours, minutes] = time.split(":").map(Number);
+    const combinedDate = new Date(date);
+    combinedDate.setHours(hours || 0, minutes || 0, 0, 0);
 
-    const combined = new Date(parsedDate.getFullYear(), parsedDate.getMonth(), parsedDate.getDate(), hours, minutes, 0, 0);
-
-    return { datetime: combined };
+    return { datetime: combinedDate };
   });
 };
